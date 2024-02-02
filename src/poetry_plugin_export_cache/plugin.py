8f2abd937a877fe9bbe0d1de484840a33357be76
Ilja Orlovs <vrghost@gmail.com>
2024-02-02 21:53:14 +0000

    bits

--- src/poetry_plugin_export_cache/plugin.py
+++ src/poetry_plugin_export_cache/plugin.py
@@ -1,6 +1,21 @@
+import cleo
 import poetry
+
+class CustomCommand(cleo.commands.command.Command):
+
+    name = "export-cache"
+
+    def handle(self) -> int:
+        self.line("My command")
+
+        return 0
+
+def factory():
+    return CustomCommand()
 
 class ExportCachePlugin(poetry.plugins.application_plugin.ApplicationPlugin):
 
-    def activate(self):
+    def activate(self, application: poetry.console.application.Application):
+        print(type(application))
         1/0
+        application.command_loader.register_factory("export cache", factory)
---
+++ tests/test_simple.py
@@ -0,0 +1,2 @@
+def test_import():
+    import poetry_plugin_export_cache
