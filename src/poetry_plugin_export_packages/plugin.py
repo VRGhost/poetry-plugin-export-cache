36d93289e4456740c263b02649f73e6413a4af68
Ilja Orlovs <vrghost@gmail.com>
2024-02-11 09:41:33 +0000

    tests

--- src/poetry_plugin_export_packages/plugin.py
+++ src/poetry_plugin_export_packages/plugin.py
@@ -2,6 +2,7 @@ import pathlib
 import shlex
 import shutil
 import tempfile
+import threading
 import typing
 
 import poetry.console.application
@@ -19,38 +20,43 @@ class ExportOutput:
     rel_root: pathlib.Path
     out_dir: pathlib.Path
     pip_commands: typing.List[typing.Tuple[str]]
+    lock: threading.Lock
 
     def __init__(self, out_dir: pathlib.Path, rel_root: pathlib.Path):
         self.rel_root = rel_root.absolute()
         self.out_dir = out_dir.absolute()
         self.pip_commands = []
+        self.lock = threading.Lock()
 
     def save_file(self, orig: pathlib.Path) -> pathlib.Path:
         """Save a file in the `out_dir`, return path to the copy"""
         idx = 1
-        out_fname = self.out_dir / orig.name
-        while out_fname.exists():
-            # Ensure output name uniqueness
-            idx += 1
-            out_fname = self.out_dir / f"{orig.stem}_{idx}{''.join(orig.suffixes)}"
-        if orig.is_file():
-            shutil.copyfile(orig, out_fname)
-        elif orig.is_dir():
-            shutil.copytree(orig, out_fname)
-        else:
-            raise NotImplementedError(orig)  # pragma: nocover
+        with self.lock:
+            out_fname = self.out_dir / orig.name
+            while out_fname.exists():
+                # Ensure output name uniqueness
+                idx += 1
+                out_fname = self.out_dir / f"{orig.stem}_{idx}{''.join(orig.suffixes)}"
+            if orig.is_file():
+                shutil.copyfile(orig, out_fname)
+            elif orig.is_dir():
+                shutil.copytree(orig, out_fname)
+            else:
+                raise NotImplementedError(orig)  # pragma: nocover
         return out_fname
 
     def to_rel_path(self, path: pathlib.Path) -> pathlib.Path:
         return path.relative_to(self.rel_root)
 
     def add_pip_command(self, cmd: typing.Iterable[str]):
-        self.pip_commands.append(tuple(cmd))
+        with self.lock:
+            self.pip_commands.append(tuple(cmd))
 
     def get_pip_script(self, shebang: str) -> str:
         out_lines = [shebang, ""]
-        for cmd in self.pip_commands:
-            out_lines.append(shlex.join(("pip",) + cmd))
+        with self.lock:
+            for cmd in self.pip_commands:
+                out_lines.append(shlex.join(("pip",) + cmd))
         return "\n".join(out_lines + ["", ""])
 
 
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import poetry.console.application
 import pytest
 
 import poetry_plugin_export_packages
@@ -63,9 +64,46 @@ class TestExportOutput:
 
 
 class TestExportCommand:
+    @pytest.fixture(autouse=True)
+    def mock_installer_cls(self, mocker):
+        installer_cls = mocker.patch("poetry.installation.installer.Installer")
+        installer_cls.return_value.run.return_value = 0
+        return installer_cls
+
     @pytest.fixture
-    def command(self):
-        return poetry_plugin_export_packages.plugin.ExportPackagesCommand()
+    def mock_installer(self, mock_installer_cls):
+        return mock_installer_cls.return_value
+
+    @pytest.fixture
+    def command(self, mocker):
+        out = poetry_plugin_export_packages.plugin.ExportPackagesCommand()
+        option_mock = mocker.patch.object(out, "option")
+        option_mock.values = {
+            "output-dir": "./testout",
+        }
+        option_mock.side_effect = lambda key, default=None: option_mock.values.get(
+            key, default
+        )
+        mocker.patch.object(out, "_io")
+        app_mock = mocker.patch.object(
+            out, "_application", spec=poetry.console.application.Application
+        )
+        app_mock.poetry.config.installer_max_workers = 1
+        return out
 
     def test_def_groups(self, command):
         assert command.default_groups == {"main"}
+
+    def test_no_out_script(self, mocker, command):
+        command.option.values.update({"output-script": None, "shebang": "#! potato"})
+        rc = command.handle()
+        assert rc == 0
+
+        command.io.write_line.assert_any_call(
+            "<info>Install script: #! potato</>", verbosity=mocker.ANY
+        )
+
+    def test_run_rc(self, mock_installer, command):
+        mock_installer.run.return_value = 4242
+        rc = command.handle()
+        assert rc == 4242
